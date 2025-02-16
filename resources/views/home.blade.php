40cd6a7d0531a4750e3c1fa1bcdf4cf3cb3b036e
mowlandcodes <mowlandgaming@gmail.com>
2025-02-16 12:44:36 +0700

    edit: modified navbar. add: added hero bg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Home Page</title>
         @vite('resources/css/app.css')
     </head>
-    <body>
-
+    <body class="h-full w-full box-border bg-zinc-100">
+        <nav class="h-auto w-full px-10 flex items-center justify-between bg-slate-200">
+            <div>
+                <img src="" alt="Logo Studi Klub">
+            </div>
+            <ul class="flex space-x-20 h-full">
+                <li class="border-4 transition-all p-5 hover:border-b-gray-500"><a href="#">Beranda</a></li>
+                <li class="border-4 transition-all p-5 hover:border-b-gray-500"><a href="#">Kursus</a></li>
+                <li class="border-4 transition-all p-5 hover:border-b-gray-500"><a href="#">Tentang Kami</a></li>
+                <li class="border-4 transition-all p-5 hover:border-b-gray-500"><a href="#">Hubungi Kami</a></li>
+            </ul>
+        </nav>
     </body>
 </html>
