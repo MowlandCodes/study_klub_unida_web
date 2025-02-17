f14e6efc21f13fad344502eb623d9d3036dfc913
mowlandcodes <mowlandgaming@gmail.com>
2025-02-17 13:16:00 +0700

    add: added navbar component, and hero-image, edit: modified home views to use navbar component

---
+++ resources/views/components/nav-bar.blade.php
@@ -0,0 +1,32 @@
+<header class="relative flex justify-between px-20 items-center border-b-2 border-gray-200 h-auto">
+    <a href="/" class="flex items-center font-bold text-xl">
+        <span class="text-blue-600 mr-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+                <g fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4">
+                    <path stroke-linecap="round" d="M22 9H11a3 3 0 0 0-3 3v21h32V22" />
+                    <path d="M4 33h40v2a6 6 0 0 1-6 6H10a6 6 0 0 1-6-6z" />
+                    <path stroke-linecap="round" d="m33 7l-4 4l4 4m6-8l4 4l-4 4" />
+                </g>
+            </svg>
+        </span>
+        <span>Study Klub</span>
+    </a>
+    <ul class="flex items-center gap-x-16">
+        <li class="relative group justify-center">
+            <a class="text-gray-700 hover:text-blue-500 block p-4 transition-all" href="#">Beranda</a>
+            <div class="absolute bottom-0 left-0 w-full h-[0.1rem] bg-blue-500 transform scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></div>
+        </li>
+        <li class="relative group justify-center">
+            <a class="text-gray-700 hover:text-blue-500 block p-4 transition-all" href="#">Kursus</a>
+            <div class="absolute bottom-0 left-0 w-full h-[0.1rem] bg-blue-500 transform scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></div>
+        </li>
+        <li class="relative group justify-center">
+            <a class="text-gray-700 hover:text-blue-500 block p-4 transition-all" href="#">Tentang Kami</a>
+            <div class="absolute bottom-0 left-0 w-full h-[0.1rem] bg-blue-500 transform scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></div>
+        </li>
+        <li class="relative group justify-center">
+            <a class="text-gray-700 hover:text-blue-500 block p-4 transition-all" href="#">Hubungi Kami</a>
+            <div class="absolute bottom-0 left-0 w-full h-[0.1rem] bg-blue-500 transform scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></div>
+        </li>
+    </ul>
+</header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,16 +7,6 @@
         @vite('resources/css/app.css')
     </head>
     <body class="h-full w-full box-border bg-zinc-100">
-        <nav class="h-auto w-full px-10 flex items-center justify-between bg-slate-200">
-            <div>
-                <img src="" alt="Logo Studi Klub">
-            </div>
-            <ul class="flex space-x-20 h-full">
-                <li class="border-4 transition-all p-5 hover:border-b-gray-500"><a href="#">Beranda</a></li>
-                <li class="border-4 transition-all p-5 hover:border-b-gray-500"><a href="#">Kursus</a></li>
-                <li class="border-4 transition-all p-5 hover:border-b-gray-500"><a href="#">Tentang Kami</a></li>
-                <li class="border-4 transition-all p-5 hover:border-b-gray-500"><a href="#">Hubungi Kami</a></li>
-            </ul>
-        </nav>
+        <x-nav-bar></x-nav-bar>
     </body>
 </html>
